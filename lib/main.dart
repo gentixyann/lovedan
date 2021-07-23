63577c03618d17d08d191a816a7ea158902f3da8
Gento <a551.return@gmail.com>
2021-07-23 22:43:14 +0900

    screen追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import './screens/top_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,15 +13,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'LoveDan',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primaryColor: Colors.pink[50],
       ),
       home: MyHomePage(),
---
+++ lib/screens/top_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class TopScreen extends StatelessWidget {
+  static const routeName = '/top';
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('start'),
+    );
+  }
+}
