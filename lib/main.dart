9a4084a457661b81268f81d2907aa2eea2db7119
Gento <a551.return@gmail.com>
2022-04-29 17:47:51 +0900

    google font 変更

--- lib/main.dart
+++ lib/main.dart
@@ -1,158 +1,82 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:lovedan/config/config.dart';
+import 'package:lovedan/providers/user_provider.dart';
+import 'package:lovedan/responsive/mobile_screen_layout.dart';
+import 'package:lovedan/responsive/responsive_layout.dart';
+import 'package:lovedan/responsive/web_screen_layout.dart';
+import 'package:lovedan/screens/introduction/introduction_screen.dart';
+import 'package:lovedan/utils/colors.dart';
 import 'package:provider/provider.dart';
-import './config/hex_color.dart';
-import './screens/top_screen.dart';
-import './screens/tabs_screen.dart';
-import './screens/my_questions/my_questions_screen.dart';
-import './screens/post_question/post_question_screen.dart';
-import './screens/settings/settings_screen.dart';
-import './screens/introduction/introduction_screen.dart';
-import './services/auth_service.dart';
-import './services/question_service.dart';
-import './services/comment_service.dart';
-import 'screens/question/question_detail_screen.dart';
-import './screens/post_comment/post_comment_screen.dart';
-import './screens/post_question/get_image_screen.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  if (kIsWeb) {
+    // webの場合
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+          apiKey: apiKey,
+          appId: appId,
+          messagingSenderId: messagingSenderId,
+          projectId: projectId,
+          storageBucket: storageBucket),
+    );
+  } else {
+    // アプリの場合
+    await Firebase.initializeApp();
+  }
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // Create the initialization Future outside of `build`:
-  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
-
+  const MyApp({Key? key}) : super(key: key);
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: _initialization,
-      builder: (context, snapshot) {
-        // Check for errors
-        if (snapshot.hasError) {
-          return Center(
-            // snapshot.errorをStringに
-            child: Text('読み込みでエラー発生' + (snapshot.error as String)),
-          );
-        }
-
-        // Once complete, show your application
-        if (snapshot.connectionState == ConnectionState.done) {
-          return MyHomePage();
-        }
-
-        // Otherwise, show something whilst waiting for initialization to complete
-        return CircularProgressIndicator();
-      },
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
     return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(
-          create: (_) => AuthService.instance(),
-        ),
-        ChangeNotifierProvider(
-          create: (_) => QuestionService(),
-        ),
-        ChangeNotifierProvider(
-          create: (_) => CommentService(),
-        ),
-      ],
-      child: MaterialApp(
-        title: 'LoveDan',
-        theme: ThemeData(
-          primaryColor: HexColor('F8EEF4'),
-          textTheme: TextTheme(
-            headline5: TextStyle(
-                fontSize: 24.0,
-                fontWeight: FontWeight.bold,
-                fontFamily: 'KiwiMaru-Regular'),
-            headline6: TextStyle(
-                fontSize: 21.0,
-                fontWeight: FontWeight.bold,
-                fontFamily: 'KiwiMaru-Regular'),
-            bodyText1: TextStyle(
-              fontSize: 16.0,
-              fontFamily: 'KiwiMaru-Regular',
-            ),
-            bodyText2: TextStyle(fontSize: 14.0),
-          ),
-          colorScheme:
-              ColorScheme.fromSwatch().copyWith(secondary: Colors.yellow[100]),
-        ),
-        home: SignProcess(),
-        routes: {
-          TopScreen.routeName: (ctx) => TopScreen(),
-          MyQuestionsScreen.routeName: (ctx) => MyQuestionsScreen(),
-          PostQuestionScreen.routeName: (ctx) => PostQuestionScreen(),
-          SettingsScreen.routeName: (ctx) => SettingsScreen(),
-          QuestionDetailScreen.routeName: (ctx) => QuestionDetailScreen(),
-          PostCommentScreen.routeName: (ctx) => PostCommentScreen(),
-          GetImageScreen.routeName: (ctx) => GetImageScreen(),
-        },
-      ),
-    );
-  }
-}
-
-class SignProcess extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Consumer(builder: (context, AuthService authService, _) {
-        // ログインの状態に応じて処理を遷移させる。
-        switch (authService.status) {
-          case Status.uninitialized:
-            print('uninitialized');
-            return Center(child: CircularProgressIndicator());
-          case Status.unauthenticated:
-            return IntroductionScreen();
-          case Status.authenticating:
-            print('anonymously');
-            return Center(
-                child: CircularProgressIndicator(
-              valueColor:
-                  AlwaysStoppedAnimation<Color>(Theme.of(context).accentColor),
-            ));
-          case Status.authenticated:
-            print("authenticated");
-            break;
-        }
-        return ViewPage();
-      }),
-    );
-  }
-}
-
-class ViewPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: StreamBuilder<User?>(
-          stream: FirebaseAuth.instance.authStateChanges(),
-          builder: (ctx, userSnapshot) {
-            if (userSnapshot.connectionState == ConnectionState.active) {
-              User? user = userSnapshot.data;
-              if (user == null) {
-                return IntroductionScreen();
+        providers: [
+          ChangeNotifierProvider(
+            create: (_) => UserProvider(),
+          )
+        ],
+        child: MaterialApp(
+          title: 'LoveDan',
+          theme: ThemeData.light().copyWith(
+              scaffoldBackgroundColor: mobileBackgroundColor,
+              textTheme: TextTheme(
+                  bodyText1: TextStyle(
+                fontFamily: 'NotoSansJP-Regular',
+              ))),
+          home: StreamBuilder(
+            stream: FirebaseAuth.instance.authStateChanges(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.active) {
+                // Checking if the snapshot has any data or not
+                if (snapshot.hasData) {
+                  // if snapshot has data which means user is logged in then we check the width of screen and accordingly display the screen layout
+                  return const ResponsiveLayout(
+                    mobileScreenLayout: MobileScreenLayout(),
+                    webScreenLayout: WebScreenLayout(),
+                  );
+                } else if (snapshot.hasError) {
+                  return Center(
+                    child: Text('${snapshot.error}'),
+                  );
+                }
               }
-              return TabsScreen();
-            } else {
-              return Scaffold(
-                body: Center(
+              // means connection to future hasnt been made yet
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(
                   child: CircularProgressIndicator(),
-                ),
-              );
-            }
-          }),
-    );
+                );
+              }
+              return IntroductionScreen();
+            },
+          ),
+        ));
   }
 }
