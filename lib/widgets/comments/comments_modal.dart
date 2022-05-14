75341d244473d1381cbd2e239d0561d8ea67cb6e
Gento <a551.return@gmail.com>
2022-05-14 13:34:11 +0900

    GestureDetectorでtextfild以外をtapでunfocusに

--- lib/widgets/comments/comments_modal.dart
+++ lib/widgets/comments/comments_modal.dart
@@ -11,54 +11,91 @@ class CommentsModal extends StatefulWidget {
 }
 
 class _CommentsModalState extends State<CommentsModal> {
+  final TextEditingController _commentController = TextEditingController();
+  final _focusNode = FocusNode();
+  var _enteredComment = '';
+
   @override
   Widget build(BuildContext context) {
     SizeConfig().init(context);
-    return Container(
-        height: SizeConfig.blockSizeVertical! * 80,
-        child: Column(
-          children: <Widget>[
-            Container(
-              margin: const EdgeInsets.symmetric(vertical: 10),
-              alignment: Alignment.center,
-              width: 50,
-              height: 5,
-              decoration: BoxDecoration(
-                color: grayColor,
-                borderRadius: BorderRadius.circular(10),
+    return GestureDetector(
+      onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+      // onTap: () => print('tappu'),
+      behavior: HitTestBehavior.opaque,
+      child: Container(
+          height: SizeConfig.blockSizeVertical! * 80,
+          child: Column(
+            children: <Widget>[
+              Container(
+                margin: const EdgeInsets.symmetric(vertical: 10),
+                alignment: Alignment.center,
+                width: 50,
+                height: 5,
+                decoration: BoxDecoration(
+                  color: grayColor,
+                  borderRadius: BorderRadius.circular(10),
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Text(
-                    'コメント',
-                    style: Theme.of(context).textTheme.headline6,
-                  ),
-                  IconButton(
-                      onPressed: () {
-                        Navigator.of(context).pop();
-                      },
-                      icon: Icon(
-                        Icons.close,
-                      )),
-                ],
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text(
+                      'コメント',
+                      style: Theme.of(context).textTheme.headline6,
+                    ),
+                    IconButton(
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                        icon: Icon(
+                          Icons.close,
+                        )),
+                  ],
+                ),
               ),
-            ),
-            const Divider(
-              height: 20,
-              thickness: 1,
-            ),
-            // コメント入力欄
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: TextField(
-                decoration: InputDecoration(hintText: 'コメントを追加'),
+              const Divider(
+                height: 20,
+                thickness: 1,
               ),
-            )
-          ],
-        ));
+              // コメント入力欄
+
+              Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.all(3.0),
+                          child: TextField(
+                            controller: _commentController,
+                            decoration: InputDecoration(labelText: 'コメントを追加'),
+                            textInputAction: TextInputAction.newline,
+                            keyboardType: TextInputType.multiline,
+                            maxLines: null,
+                            style: TextStyle(
+                              color: regularTextColor,
+                            ),
+                            onChanged: (value) {
+                              setState(() {
+                                _enteredComment = value;
+                              });
+                            },
+                          ),
+                        ),
+                      ),
+                      IconButton(
+                          onPressed: _enteredComment.trim().isEmpty
+                              ? null
+                              : () {
+                                  print('submit');
+                                },
+                          icon: Icon(Icons.send))
+                    ],
+                  ))
+            ],
+          )),
+    );
   }
 }
